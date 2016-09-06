d4678cd9125a25bbfa3976d6cb181abe33780c6a
mthomp32 <mthomp32@live.spcollege.edu>
2016-09-05 23:11:48 -0400

    Week 3 Assignment

---
+++ app/src/main/java/com/bignerdranch/android/personaltrainer/CustomerListActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.personaltrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_customer_list);
+    }
+}
